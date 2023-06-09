b1ddb582bc687cf5d1a6b64ea5c97de572f82200
lyly <lyly.lou2013@gmail.com>
2023-06-09 17:04:43 +0200

    up form admin

--- src/Form/TutorialType.php
+++ src/Form/TutorialType.php
@@ -2,10 +2,15 @@
 
 namespace App\Form;
 
+use DateTimeImmutable;
 use App\Entity\Tutorial;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class TutorialType extends AbstractType
 {
@@ -13,15 +18,34 @@ class TutorialType extends AbstractType
     {
         $builder
             ->add('tutoName')
-            ->add('tutoDescription')
-            ->add('tutoFileName')
-            ->add('tutoVideoName')
-            ->add('tutoImageName')
+            ->add('tutoDescription', CKEditorType::class)
+            ->add('tutoFileName', FileType::class,[
+                'required' => false,
+                'label' => 'Image du document'
+                ])
+            ->add('tutoVideoName', FileType::class,[
+                'required' => false,
+                'label' => 'Image de la video'
+                ])
+            ->add('tutoImageName', FileType::class,[
+                'required' => false,
+                'label' => 'Image du tuto'
+                ])
             ->add('tutoSupportType')
-            ->add('tutoSlug')
-            ->add('tutoUpdatedAt')
+            //->add('tutoSlug')
+            ->add('tutoUpdatedAt', DateTimeType::class, [
+                'widget'=>'single_text',
+                'data'=>new DateTimeImmutable(),
+            ])
             ->add('tutoFavoris')
-            ->add('categories')
+            ->add('categories', EntityType::class, [
+                'class'=> 'App\Entity\Category',
+                'multiple'=> true,
+                'attr'=>[
+                    "class"=>"select2",
+                    "id"=>"select2-tutorials",
+                ]
+            ])
         ;
     }
 
