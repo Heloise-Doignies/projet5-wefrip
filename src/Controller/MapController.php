460b69058cd1650be62ba7baaa306446c38c0e0d
enkhe-D <enkhedeny07@gmail.com>
2023-06-12 18:32:12 +0200

    ajout de la map

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -8,11 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MapController extends AbstractController
 {
-    #[Route('/map', name: 'app_map')]
+    #[Route("/map", name: "app_map")]
     public function index(): Response
     {
-        return $this->render('map/index.html.twig', [
-            'controller_name' => 'MapController',
+        $latitude = 48.85;
+        $longitude = 2.349903;
+        $popupContent = "";
+        $zoom = 19;
+        $markers = [
+            ["lat" => 48.85, "lng" => 2.349903, "ok" => "contenue"],
+/*             ["lat" => 48.862395726039516, "lng" => 2.399574555185428, "popupContent" => 'cacacacaca'], */
+        ];
+
+        return $this->render("map/index.html.twig", [
+            "controller_name" => "WeFrip'",
+            "latitude" => $latitude,
+            "longitude" => $longitude,
+            "popupContent" => $popupContent,
+            "zoom" => $zoom,
+            "markers" => $markers,
         ]);
     }
 }
