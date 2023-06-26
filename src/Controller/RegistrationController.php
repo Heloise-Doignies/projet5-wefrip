004a3dc33a2b63ddf8f6e877a539a3cc36fd21c8
enkhe-D <enkhedeny07@gmail.com>
2023-06-26 17:31:52 +0200

    ajout de la possibilité de modifier le mot de passe

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -53,7 +53,7 @@ class RegistrationController extends AbstractController
                 (new TemplatedEmail())
                     ->from(new Address('admin@wefrip.com', 'We frip'))
                     ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
+                    ->subject('Veuillez confirmer votre adresse e-mail')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
