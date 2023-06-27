8b38b47930b3e8131125289a4cac861b989f5e0a
HD <heloise.dgn@gmail.com>
2023-06-27 14:17:12 +0200

    avatar ajout

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -79,6 +79,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\OneToMany(mappedBy: 'creator', targetEntity: Event::class)]
     private Collection $eventCreator;
 
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?Avatar $avatar = null;
+
     public function __construct()
     {
         $this->tutorials = new ArrayCollection();
@@ -223,31 +226,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->avatarName = $avatarName;
 
         return $this;
-    }
-    public function getAvatarFile(): ?File
-    {
-        return $this->avatarFile;
-    }
-
-    /**
-     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
-     * of 'UploadedFile' is injected into this setter to trigger the update. If this
-     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
-     * must be able to accept an instance of 'File' as the bundle will inject one here
-     * during Doctrine hydration.
-     *
-     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $avatarFile
-     */
-    
-    public function setAvatarFile(?File $avatarFile = null): void
-    {
-        $this->avatarFile = $avatarFile;
-
-        if (null !== $avatarFile) {
-            // It is required that at least one field changes if you are using doctrine
-            // otherwise the event listeners won't be called and the file is lost
-            $this->userUpdatedAt = new \DateTimeImmutable();
-        }
     }
 
     public function getLastname(): ?string
@@ -399,6 +377,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
                 $eventCreator->setCreator(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getAvatar(): ?Avatar
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(?Avatar $avatar): static
+    {
+        $this->avatar = $avatar;
 
         return $this;
     }
